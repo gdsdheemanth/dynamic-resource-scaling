29720f1a5f17d48b7db4d875a7d5bf57c5c7998f
gdsdheemanth <dheemanth.gds@gmail.com>
2025-02-02 20:32:46 +0530

    changes the cluster create param to zone

--- scripts/deploy_k8s_cluster.sh
+++ scripts/deploy_k8s_cluster.sh
@@ -6,10 +6,10 @@
 #     --enable-autoscaling --min-nodes=1 --max-nodes=5
 
 gcloud container clusters create "ml-cluster" \
-    --num-nodes=2 --machine-type=e2-standard-4 --region=us-central1
+    --num-nodes=2 --machine-type=e2-standard-4 --zone=us-central1-a
 
 # Get credentials
-gcloud container clusters get-credentials "ml-cluster" --region=us-central1
+gcloud container clusters get-credentials "ml-cluster" --zone=us-central1-a
 
 # Create Namespaces
 kubectl apply -f config/k8s_config.yaml
